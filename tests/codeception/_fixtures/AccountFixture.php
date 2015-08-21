48d8854fc0055f239e479040dc9870551e60890a
Will Wharton <willmwharton@gmail.com>
2015-08-20 21:30:09 -0700

    maybe i should remove the generated?

---
+++ tests/codeception/_fixtures/AccountFixture.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\codeception\_fixtures;
+
+use yii\test\ActiveFixture;
+
+class AccountFixture extends ActiveFixture
+{
+    public $modelClass = 'wartron\yii2account\models\Account';
+}
