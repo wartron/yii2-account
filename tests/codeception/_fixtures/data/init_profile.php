928ea7e279f2c5ff8512b5c67a76be6fef7c3139
Will Wharton <willmwharton@gmail.com>
2015-08-22 12:32:49 -0700

    fixtures for uuid tests

--- tests/codeception/_fixtures/data/init_profile.php
+++ tests/codeception/_fixtures/data/init_profile.php
@@ -4,7 +4,7 @@ $time = time();
 
 return [
     'account' => [
-        'account_id' => 1,
+        'account_id' => hex2bin('11f24b97aade11e2aced000c29ae5e1b'),
         'name'    => 'John Doe',
     ],
 ];
--- tests/codeception/_fixtures/data/init_token.php
+++ tests/codeception/_fixtures/data/init_token.php
@@ -6,27 +6,27 @@ $time = time();
 
 return [
     'confirmation' => [
-        'account_id'    => 2,
-        'code'       => 'NO2aCmBIjFQX624xmAc3VBu7Th3NJoa6',
-        'type'       => Token::TYPE_CONFIRMATION,
-        'created_at' => $time,
+        'account_id'    =>  hex2bin('22f24b97aade11e2aced000c29ae5e1b'),
+        'code'          =>  'NO2aCmBIjFQX624xmAc3VBu7Th3NJoa6',
+        'type'          =>  Token::TYPE_CONFIRMATION,
+        'created_at'    =>  $time,
     ],
     'expired_confirmation' => [
-        'account_id'    => 3,
-        'code'       => 'qxYa315rqRgCOjYGk82GFHMEAV3T82AX',
-        'type'       => Token::TYPE_CONFIRMATION,
-        'created_at' => $time - 86401,
+        'account_id'    =>  hex2bin('33f24b97aade11e2aced000c29ae5e1b'),
+        'code'          =>  'qxYa315rqRgCOjYGk82GFHMEAV3T82AX',
+        'type'          =>  Token::TYPE_CONFIRMATION,
+        'created_at'    =>  $time - 86401,
     ],
     'expired_recovery' => [
-        'account_id'    => 5,
-        'code'       => 'a5839d0e73b9c525942c2f59e88c1aaf',
-        'type'       => Token::TYPE_RECOVERY,
-        'created_at' => $time - 21601,
+        'account_id'    =>  hex2bin('55f24b97aade11e2aced000c29ae5e1b'),
+        'code'          =>  'a5839d0e73b9c525942c2f59e88c1aaf',
+        'type'          =>  Token::TYPE_RECOVERY,
+        'created_at'    =>  $time - 21601,
     ],
     'recovery' => [
-        'account_id'    => 6,
-        'code'       => '6f5d0dad53ef73e6ba6f01a441c0e602',
-        'type'       => Token::TYPE_RECOVERY,
-        'created_at' => $time,
+        'account_id'    =>  hex2bin('66f24b97aade11e2aced000c29ae5e1b'),
+        'code'          =>  '6f5d0dad53ef73e6ba6f01a441c0e602',
+        'type'          =>  Token::TYPE_RECOVERY,
+        'created_at'    =>  $time,
     ],
 ];
