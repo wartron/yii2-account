458b9cb66cdc68fda7b4a5e20c1c069bcdf508da
Will Wharton <willmwharton@gmail.com>
2015-08-23 17:22:59 -0700

    use a detailview for the admin/info view

--- views/admin/_info.php
+++ views/admin/_info.php
@@ -14,42 +14,43 @@
  * @var wartron\yii2account\models\User
  */
 
+use yii\grid\GridView;
+use yii\widgets\DetailView;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->beginContent('@wartron/yii2account/views/admin/update.php', ['user' => $user]);
 
-?>
 
-<table class="table">
-    <tr>
-        <td><strong><?= Yii::t('account', 'Registration time') ?>:</strong></td>
-        <td><?= Yii::t('account', '{0, date, MMMM dd, YYYY HH:mm}', [$user->created_at]) ?></td>
-    </tr>
-    <?php if ($user->registration_ip !== null): ?>
-        <tr>
-            <td><strong><?= Yii::t('account', 'Registration IP') ?>:</strong></td>
-            <td><?= $user->registration_ip ?></td>
-        </tr>
-    <?php endif ?>
-    <tr>
-        <td><strong><?= Yii::t('account', 'Confirmation status') ?>:</strong></td>
-        <?php if ($user->isConfirmed): ?>
-            <td class="text-success"><?= Yii::t('account', 'Confirmed at {0, date, MMMM dd, YYYY HH:mm}', [$user->confirmed_at]) ?></td>
-        <?php else: ?>
-            <td class="text-danger"><?= Yii::t('account', 'Unconfirmed') ?></td>
-        <?php endif ?>
-    </tr>
-    <tr>
-        <td><strong><?= Yii::t('account', 'Block status') ?>:</strong></td>
-        <?php if ($user->isBlocked): ?>
-            <td class="text-danger"><?= Yii::t('account', 'Blocked at {0, date, MMMM dd, YYYY HH:mm}', [$user->blocked_at]) ?></td>
-        <?php else: ?>
-            <td class="text-success"><?= Yii::t('account', 'Not blocked') ?></td>
-        <?php endif ?>
-    </tr>
-</table>
 
-<?php
+echo DetailView::widget([
+    'model'         =>  $user,
+    'attributes'    =>  [
+        'id:hex',
+        'username',
+        'email',
+        [
+            'label' =>  Yii::t('account', 'Registration time'),
+            'type'  =>  'raw',
+            'value' =>  Yii::t('account', '{0, date, MMMM dd, YYYY HH:mm}', [$user->created_at]),
+        ],
+        [
+            'label' =>  Yii::t('account', 'Confirmation status'),
+            'type'  =>  'raw',
+            'value' =>  $user->isConfirmed ?
+                Yii::t('account', 'Confirmed at {0, date, MMMM dd, YYYY HH:mm}', [$user->confirmed_at]) :
+                Yii::t('account', 'Unconfirmed'),
+        ],
+        [
+            'label' =>  Yii::t('account', 'Block status'),
+            'type'  =>  'raw',
+            'value' =>  $user->isBlocked ?
+                Yii::t('account', 'Blocked at {0, date, MMMM dd, YYYY HH:mm}', [$user->blocked_at]) :
+                Yii::t('account', 'Not blocked'),
+        ],
+        'registration_ip',
+    ],
+]);
+
 
 $this->endContent();
-
-?>
